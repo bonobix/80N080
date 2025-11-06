b0ccf507fb76a5292c4fe9e68f8f192c69048c72
Eduard P. <144683920+bonobix@users.noreply.github.com>
2025-11-06 10:53:03 +0100

    Update update_badges.py

--- update_badges.py
+++ update_badges.py
@@ -3,7 +3,7 @@ import re
 
 USERNAME = "eduard-platon.6f312b2b" 
 README_PATH = "README.md"
-IMG_WIDTH = 75 
+IMG_WIDTH = 50 
 
 # Recupera dati dei badge
 url = f"https://www.credly.com/users/{USERNAME}/badges.json"
